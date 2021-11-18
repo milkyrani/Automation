b2f08ee4df45cb6eddfbd902e492701f8df51513
Milky Rani <milky.rani@globallogic.com>
2021-11-18 17:49:40 +0530

    File changes done by milky

---
+++ src/training/hello.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package training;
+
+/**
+ * @author milky.rani
+ *
+ */
+public class hello {
+
+	/**
+	 * 
+	 */
+	public hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
